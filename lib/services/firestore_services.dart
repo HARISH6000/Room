48271c30c886f14de3d17a0d1e6f1a06a4cc86cc
unknown <harishgovind6000@gmail.com>
2023-12-28 13:18:05 +0530

    Added new services: addUsr()

--- lib/services/firestore_services.dart
+++ lib/services/firestore_services.dart
@@ -1,11 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:room/models/room_model.dart';
 import 'package:room/models/message_model.dart';
+import 'package:room/models/user_model.dart';
 
 class FirebaseFirestoreServices {
-  final String rid;
-  const FirebaseFirestoreServices({required this.rid});
+  String? rid = null;
+  FirebaseFirestoreServices({this.rid});
   Future<void> addMsg(String msg, String uid) async {
+    if (rid == null) {
+      return;
+    }
     FirebaseFirestore db = FirebaseFirestore.instance;
     final message = Message(msg: msg, ts: Timestamp.now(), uid: uid);
     await db
@@ -14,5 +18,13 @@ class FirebaseFirestoreServices {
         .collection("Message")
         .doc()
         .set(message.toFirestore());
+  }
+
+  Future<void> addUsr(String? uid, String uname) async {
+    if (uid != null) {
+      FirebaseFirestore db = FirebaseFirestore.instance;
+      final user = User(uname: uname);
+      await db.collection("User").doc(uid).set(user.toFirestore());
+    }
   }
 }
